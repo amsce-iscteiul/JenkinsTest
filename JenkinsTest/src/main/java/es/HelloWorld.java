2e0b279626d00ec35962e039814610c96844fe89
Menoita99 <rmsma1@iscte-iul.pt>
2020-05-27 00:07:44 +0100

    commit

---
+++ JenkinsTest/src/main/java/es/HelloWorld.java
@@ -0,0 +1,16 @@
+package es;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println(teste());
+	}
+
+	public static String teste() {
+		return "Jenkins Test Success!!";
+	}
+	
+	public String testIntance() {
+		return "Success";
+	}
+}
